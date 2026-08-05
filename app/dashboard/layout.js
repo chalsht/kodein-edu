9786b6a04e5dc5439b91379591a84b305a9241ed
chalsht <chaca9266@gmail.com>
2026-08-05 10:58:56 +0700

    Update tampilan dashboard admin

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -19,9 +19,9 @@ export default function DashboardLayout({ children }) {
     <div className="flex min-h-screen">
       <SidebarAdmin />
 
-      <main className="flex-1 bg-slate-100 p-5 md:p-10 pt-20 md:pt-10">
-        {children}
-      </main>
+<main className="flex-1 min-h-screen bg-slate-100 p-5 md:p-10 pt-20 md:pt-10">
+  {children}
+</main>
     </div>
   );
 }
