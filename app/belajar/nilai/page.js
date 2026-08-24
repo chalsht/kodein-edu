4e76c13d4460bf5b34869e5a0da4636ffaaef2ce
chalsht <chaca9266@gmail.com>
2026-08-24 12:53:53 +0700

    Update

--- app/belajar/nilai/page.js
+++ app/belajar/nilai/page.js
@@ -1,100 +1,43 @@
 "use client";
 
-// ==========================================
-// HALAMAN NILAI
-// ==========================================
-
 import { useEffect, useState } from "react";
 
 export default function Nilai() {
+  const [nilai, setNilai] = useState([]);
 
-  const [nilai,setNilai]=useState([]);
-
-  useEffect(()=>{
-
-    const load=async()=>{
-
-      const email=localStorage.getItem("email");
-
-      const res=await fetch(`/api/nilai?email=${email}`);
-
-      const data=await res.json();
-
-      setNilai(data);
-
+  useEffect(() => {
+    const load = async () => {
+      const email = localStorage.getItem("email");
+      const res = await fetch(`/api/nilai?email=${email}`);
+      setNilai(await res.json());
     };
 
     load();
+  }, []);
 
-  },[]);
-
-  return(
-
+  return (
     <div className="p-10">
-
-      <h1 className="text-4xl font-bold mb-8">
-
-        Nilai Quiz
-
-      </h1>
+      <h1 className="text-4xl font-bold mb-8">Nilai Quiz</h1>
 
       <table className="w-full bg-white shadow rounded-xl">
-
         <thead className="bg-slate-800 text-white">
-
           <tr>
-
             <th className="p-3">No</th>
-
             <th>Materi</th>
-
             <th>Nilai</th>
-
           </tr>
-
         </thead>
 
         <tbody>
-
-          {
-
-            nilai.map((item,index)=>(
-
-              <tr
-              key={item.id}
-              className="border-b"
-              >
-
-                <td className="p-3">
-
-                  {index+1}
-
-                </td>
-
-                <td>
-
-                  {item.judul}
-
-                </td>
-
-                <td>
-
-                  {item.nilai}
-
-                </td>
-
-              </tr>
-
-            ))
-
-          }
-
+          {nilai.map((item, index) => (
+            <tr key={item.id} className="border-b">
+              <td className="p-3">{index + 1}</td>
+              <td>{item.judul}</td>
+              <td>{item.nilai}</td>
+            </tr>
+          ))}
         </tbody>
-
       </table>
-
     </div>
-
   );
-
 }
